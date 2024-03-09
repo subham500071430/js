ab8e81baf77f3dc5592bca72e62d394fff3c3a4a
subham <subham10022000@gmail.com>
2024-03-10 04:08:42 +0530

    datatype, loops and string

---
+++ topics/src/main.js
@@ -0,0 +1,33 @@
+//loops
+/*
+for(let i=0;i<5;i++)
+{
+    for(let j =0;j<=i;j++){
+        console.log("*")
+    }
+    console.log("")
+}
+
+let student = {name:"subham",age:20}
+
+for(let key in student){
+    console.log(student[key])
+}
+
+let num = 10
+
+let guess = prompt("Guess the number")
+
+while (num != guess){
+    guess = prompt("Wrong Guess Guess again")
+}
+
+console.log("you have guessed correct number")
+*/
+//template literals
+
+// A way to have embedded expressions in string
+let a = `sum of ${1} + ${2} = ${1+2}`
+console.log(a)
+
+
